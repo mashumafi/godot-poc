4761b28c8428e53ea4065506fd3d597e8c485134
Matthew Murphy <mashumafi@gmail.com>
2018-05-29 21:29:35 -0400

    cell callback

--- scripts/edit_scene.gd
+++ scripts/edit_scene.gd
@@ -6,6 +6,10 @@ func _ready():
     $edit_panel/edit_box/width_box/width_sb.connect("value_changed", self, "width_sb_pressed")
     $edit_panel/edit_box/height_box/height_sb.connect("value_changed", self, "height_sb_pressed")
 
+    $maze.connect("cell_changed", self, "cell_changed")
+    $maze.set_width($edit_panel/edit_box/width_box/width_sb.value);
+    $maze.set_height($edit_panel/edit_box/height_box/height_sb.value);
+
 func width_sb_pressed(value):
     $maze.set_width(value)
 
@@ -16,3 +20,6 @@ func _notification(what):
     match what:
         MainLoop.NOTIFICATION_WM_QUIT_REQUEST, MainLoop.NOTIFICATION_WM_GO_BACK_REQUEST:
             get_parent_control().back()
+
+func cell_changed(cell):
+    print(cell)
