1c6fd3d5f157cbc93602a1283fd17cd10089077e
Matthew Murphy <mashumafi@gmail.com>
2018-06-04 07:30:59 -0400

    draw obstacle

--- scripts/edit_scene.gd
+++ scripts/edit_scene.gd
@@ -6,7 +6,7 @@ var g = Canvas.new()
 var generator = ObstacleGenerator.new()
 onready var obstacle_list = $edit_panel/edit_box/obstacle_list
 onready var preview = $edit_panel/edit_box/preview_box/preview
-onready var maze = $maze_container/maze
+onready var maze = $maze
 
 func _ready():
     $edit_panel/edit_box/width_box/width_sb.connect("value_changed", self, "width_sb_pressed")
@@ -15,7 +15,6 @@ func _ready():
     maze.connect("cell_changed", self, "cell_changed")
     maze.set_width($edit_panel/edit_box/width_box/width_sb.value)
     maze.set_height($edit_panel/edit_box/height_box/height_sb.value)
-
 
     preview.connect("draw", self, "preview_draw")
     g.set_canvas(preview)
@@ -37,11 +36,13 @@ func _notification(what):
 
 func obstacle_selected(index):
     print(obstacle_list.get_item_text(index))
+    if selected:
+        selected.set_obstacle(generator.create(obstacle_list.get_item_text(index)))
+        maze.update()
 
 func preview_draw():
     if selected:
-        var ob = selected.get_obstacle()
-        ob.draw(g)
+        selected.get_obstacle().draw(g)
 
 func cell_changed(cell):
     selected = cell
